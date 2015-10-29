a34678ff2161846abcab3c9c22986d6ea7664f32
tracychu <tracyschu@gmail.com>
2015-10-29 11:37:14 -0700

    Fix conflicts

--- Fraction Tiles/CorrectViewController.swift
+++ Fraction Tiles/CorrectViewController.swift
@@ -19,7 +19,7 @@ class CorrectViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }/Users/t0chu/Documents/CodePath
+    }
     
 
     /*
